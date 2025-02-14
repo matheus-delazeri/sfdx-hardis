bdd208e261323fa7d5912712088b6ed0a070e752
Matheus Delazeri <matheusdelazeris@gmail.com>
2025-02-14 16:29:42 -0300

    Feat: create command 'flow2pdf'

---
+++ src/commands/hardis/doc/flow2pdf.ts
@@ -0,0 +1,122 @@
+/* jscpd:ignore-start */
+import { SfCommand, Flags } from '@salesforce/sf-plugins-core';
+import c from "chalk";
+import * as path from "path";
+import fs from "fs-extra";
+import { Messages } from '@salesforce/core';
+import { AnyJson } from '@salesforce/ts-types';
+import { WebSocketClient } from '../../../common/websocketClient.js';
+import { isCI, uxLog } from '../../../common/utils/index.js';
+import { MetadataUtils } from '../../../common/metadata-utils/index.js';
+import { generateFlowMarkdownFile, generateHistoryDiffMarkdown, generateMarkdownFileWithMermaid } from '../../../common/utils/mermaidUtils.js';
+import { CONSTANTS } from '../../../config/index.js';
+import { mdToPdf } from 'md-to-pdf';
+
+
+Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
+const messages = Messages.loadMessages('sfdx-hardis', 'org');
+
+export default class Flow2Markdown extends SfCommand<any> {
+  public static title = 'Flow to PDF';
+
+  public static description = `Generates a Markdown and PDF documentation from a Flow file
+  
+If [AI integration](${CONSTANTS.DOC_URL_ROOT}/salesforce-ai-setup/) is configured, documentation will contain a summary of the Flow.  
+  `;
+
+  public static examples = [
+    '$ sf hardis:doc:flow2pdf',
+    '$ sf hardis:doc:flow2pdf --inputfile force-app/main/default/flows/MyFlow.flow-meta.xml'
+  ];
+
+  public static flags: any = {
+    inputfile: Flags.string({
+      char: 'x',
+      description: 'Path to Flow metadata file. If not specified, the command will prompt the user',
+    }),
+    outputfile: Flags.string({
+      char: 'f',
+      description: 'Force the path and name of output markdown file. Must end with .pdf',
+    }),
+    "with-history": Flags.boolean({
+      default: false,
+      description: "Generate a markdown file with the history diff of the Flow",
+    }),
+    debug: Flags.boolean({
+      char: 'd',
+      default: false,
+      description: messages.getMessage('debugMode'),
+    }),
+    websocket: Flags.string({
+      description: messages.getMessage('websocket'),
+    }),
+    skipauth: Flags.boolean({
+      description: 'Skip authentication check when a default username is required',
+    }),
+  };
+
+  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+  public static requiresProject = false;
+
+  protected withHistory = false;
+  protected inputFile;
+  protected outputFile;
+  protected debugMode = false;
+  /* jscpd:ignore-end */
+
+  public async run(): Promise<AnyJson> {
+    const { flags } = await this.parse(Flow2Markdown);
+    this.inputFile = flags.inputfile || null;
+    this.outputFile = flags.outputfile || null;
+    this.withHistory = flags["with-history"] === true ? true : false;
+    this.debugMode = flags.debug || false;
+
+    if (this.inputFile === null && !isCI) {
+      this.inputFile = await MetadataUtils.promptFlow();
+    }
+    if (!this.outputFile) {
+      await fs.ensureDir(path.join("docs", "flows"));
+      this.outputFile = path.join("docs", "flows", path.basename(this.inputFile).replace(".flow-meta.xml", ".md"));
+    }
+
+    uxLog(this, c.grey(`Generating markdown for Flow ${this.inputFile}...`));
+    const flowXml = (await fs.readFile(this.inputFile, "utf8")).toString();
+    const genRes = await generateFlowMarkdownFile(path.basename(this.inputFile, ".flow-meta.xml"), flowXml, this.outputFile, { collapsedDetails: false, describeWithAi: true });
+    if (!genRes) {
+      throw new Error("Error generating markdown file");
+    }
+    if (this.debugMode) {
+      await fs.copyFile(this.outputFile, this.outputFile.replace(".md", ".mermaid.md"));
+    }
+    const gen2res = await generateMarkdownFileWithMermaid(this.outputFile, this.outputFile, ['cli']);
+    if (!gen2res) {
+      throw new Error("Error generating mermaid markdown file");
+    }
+
+    if (this.withHistory) {
+      try {
+        await generateHistoryDiffMarkdown(this.inputFile, this.debugMode);
+      } catch (e: any) {
+        uxLog(this, c.yellow(`Error generating history diff markdown: ${e.message}`));
+      }
+    }
+
+    await mdToPdf({ path: this.outputFile }, { 
+      dest: this.outputFile.replace('.md', '.pdf'), 
+      css: `img {
+              max-width: 30%; /* Adjust this to resize all images */
+              display: block;
+              margin: 0 auto;
+            }`,
+      stylesheet_encoding: 'utf-8'
+      }
+    );
+
+    // Open file in a new VsCode tab if available
+    WebSocketClient.requestOpenFile(this.outputFile);
+
+    // Return an object to be displayed with --json
+    return { outputFile: this.outputFile };
+  }
+
+}
